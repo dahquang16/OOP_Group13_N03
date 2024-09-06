34cd6cb134d755213511142d492cc31cdfb321e3
chiduongkim <150871362+chiduongkim@users.noreply.github.com>
2024-09-06 11:39:35 +0700

    Update Book.java

--- Practice2/main/java/com/mycompany/Book.java
+++ Practice2/main/java/com/mycompany/Book.java
@@ -3,14 +3,14 @@ public class Book {
  String title;
  String author;
  int numPages;
- Book() { } ; // default constructor
+ Book() { } ;
  public Book(String t, String a, int p) {
  title = t;
  author = a;
  numPages = p;
  }
  public static void main(String[] args) {
- Book myObj = new Book("a","b",2); //New book
+ Book myObj = new Book("a","b",2); 
  System.out.println(myObj.title);
  }
 }
