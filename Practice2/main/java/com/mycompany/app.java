e6e73c5e2c70bfc28f4512cf875f47cfd8cfcf29
chiduongkim <150871362+chiduongkim@users.noreply.github.com>
2024-09-06 11:38:36 +0700

    Update app.java

--- Practice2/main/java/com/mycompany/app.java
+++ Practice2/main/java/com/mycompany/app.java
@@ -2,17 +2,17 @@ package com.mycompany.java;
 
 public class App {
 	 public static void main(String[] args) {
-	        Book myBook = new Book("Duong", "Le Duong", 68);
+	        Book myBook = new Book("Quang", "Danh Quang", 68);
 	        System.out.println("Book title: " + myBook.title);
 	        System.out.println("Author: " + myBook.author);
 	        System.out.println("Number Page: " + myBook.numPages);
 
 	        NNCollection collection = new NNCollection();
 
-	        collection.insert(new NameNumber("Duong", "23010351"));
-	        collection.insert(new NameNumber("Nga", "22098865"));
-	        System.out.println("Duong number: " + collection.findNumber("Duong"));
-	        System.out.println("Nga number: " + collection.findNumber("Nga"));
+	        collection.insert(new NameNumber("Quang", "23010230"));
+	        collection.insert(new NameNumber("Chi", "23010831"));
+	        System.out.println("Quang number: " + collection.findNumber("Quang"));
+	        System.out.println("Chi number: " + collection.findNumber("Chi"));
 
 	        Recursion recursion = new Recursion();
 	        System.out.println("Factorial: " + recursion.factorial(4));
